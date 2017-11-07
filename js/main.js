26984a0bf752bcaf882b4b4f954146dd5ab7620a
NRothera <31472463+NRothera@users.noreply.github.com>
2017-11-07 12:03:29 +0000

    fixed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
 // Global Variables ---------------------------------------------------------
 window.onload=function(){
   $('#bomberman').addClass('down');
+  $('#bomberman2').addClass('down')
   drawMap()
 }
 var breakableIndexCurrent = 0;
 var player1Score = 0;
+var fromTop2 = 1;
+var fromLeft2 = 38
+var index2 = 7;
 var fromLeft = 0;
-var fromTop = 0;
+var fromTop = 1;
 var index = 0
 var score = 0
 var arrayIndex = 0
@@ -56,164 +60,94 @@ function drawMap(){
       }else if (mapArray[arrayIndex] === 1){
         $('#container').append('<div class="breakable" id= block'+breakableIndex[breakableIndexCurrent]+ '></div>')
         breakableIndexCurrent ++
-        console.log(breakableIndexCurrent)
       }else if (mapArray[arrayIndex] === 2){
         $('#container').append('<div class="unbreakable" ></div>')
       }
     }
-    console.log($('.breakable'))
   }
 }
 
 //Character movement and boundaries ------------------------------------------------
-function processWalk(dir, player){
-
+function processWalk(dir){
+  charStep ++;
+  if (charStep === 5) charStep = 1;
 
   //removes class
-  if ( player === 'bomberman'){
-    charStep ++;
-    if (charStep === 5) charStep = 1;
+  $('#bomberman').removeAttr('class');
 
-    $('#bomberman').removeAttr('class');
-
-    //add class for direction moving
-    switch(charStep){
-      case 1: $('#bomberman').addClass(dir); break;
-      case 2: $('#bomberman').addClass(dir + '-one'); break;
-      case 3: $('#bomberman').addClass(dir); break;
-      case 4: $('#bomberman').addClass(dir + '-two'); break;
-    }
-
-    switch(dir){
-      case 'front':
-      if ($('#bomberman').position().top < 370){
-        $('#bomberman').animate({top: '+=10'}, charSpeed);
-        fromTop ++
-        //If the player steps down into a new row, add 8 to the index
-        if (fromTop % 5 === 0){
-          index += 8;
-        }
-        isIndexBlock()
-      }
-      break;
-      case 'back':
-      if ($('#bomberman').position().top > 0){
-        $('#bomberman').animate({top: '-=10'}, charSpeed);
-        fromTop --
-        //if the player steps up into a new row, take 8 off the index
-        if (fromTop %5 === 4){
-          index -=8
-        }
-        isIndexBlock()
-        }
-        break;
-      case 'left':
-      if ($('#bomberman').position().left >0){
-        $('#bomberman').animate({left:'-=10'}, charSpeed);
-        fromLeft --
-        //if the player steps left into a column, take 1 off the index
-        if (fromLeft %5 === 4){
-          index --
-        }
-        isIndexBlock()
-      }
-      break;
-      case 'right':
-      if ($('#bomberman').position().left <380){
-        $('#bomberman').animate({left: '+=10'}, charSpeed);
-        fromLeft ++
-        console.log(fromLeft)
-        //if player steps right into a column, add 1 to the index
-        if (fromLeft % 5 ===0){
-          index ++
-        }
-        console.log(index)
-        isIndexBlock()
-      }
-      break;
-    }
-  }else{
-    charStep2 ++;
-    if (charStep2 === 5) charStep2=1;
-    $('#bomberman2').removeAttr('class');
-
-    switch (charStep2) {
-        case 1: $('#bomberman2').addClass(dir+'2'); break;
-        case 2: $('#bomberman2').addClass(dir+'-one2'); break;
-        case 3: $('#bomberman2').addClass(dir+'2'); break;
-        case 4: $('#bomberman2').addClass(dir+'-two2');  break;
-      }
-      switch(dir) {
-      case 'front':
-      if ($('#bomberman2').position().top <370){
-          $('#bomberman2').animate({top: '+=10'}, charSpeed);
-          fromTop2 ++
-          if (fromTop2 % 5 ===0){
-            index2 +=8
-          }
-          isIndexBlock2()
-        }
-        break;
-      case 'back':
-        //don't let the character move any further up if they are already at the top of the screen
-        if ($('#bomberman2').position().top > 0) {
-          $('#bombermn2').animate({top: '-=10'}, charSpeed2);
-          fromTop2 --
-          console.log(fromTop2)
-          if (fromTop2 % 5 === 4){
-            index2 -=8
-          }
-          isIndexBlock2();
-          console.log(index2)
-        }
-        break;
-      case 'left':
-      //don't let the character move any further left if they are already at the left side of the screen
-      if ($('#bomberman2').position().left > 0) {
-          $('#bomberman2').animate({left: '-=10'}, charSpeed2);
-          fromLeft2 --
-          console.log(fromLeft2)
-          if (fromLeft2 %5 ===4){
-            index2 --
-          }
-          console.log(index2)
-          isIndexBlock2()
-        }
-        break;
-      case 'right':
-      if ($('#bomberman2').position().left <380){
-        $('#bomberman2').animate({left: '+=10'}, charSpeed2);
-        fromLeft2 ++
-        console.log(fromLeft2)
-        if (fromLeft%5===0){
-          index2++
-        }
-        console.log(index2)
-        isIndexBlock2
-      }
-        break;
-      }
-    }
+  //add class for direction moving
+  switch(charStep){
+    case 1: $('#bomberman').addClass(dir); break;
+    case 2: $('#bomberman').addClass(dir + '-one'); break;
+    case 3: $('#bomberman').addClass(dir); break;
+    case 4: $('#bomberman').addClass(dir + '-two'); break;
+  }
 
   //move bomberman length and height of image
-
+  switch(dir){
+    case 'front':
+    if ($('#bomberman').position().top < 370){
+      $('#bomberman').animate({top: '+=10'}, charSpeed);
+      fromTop ++
+      //If the player steps down into a new row, add 8 to the index
+      if (fromTop % 5 === 0){
+        index += 8;
+      }
+      isIndexBlock()
+    }
+    break;
+    case 'back':
+    if ($('#bomberman').position().top > 0){
+      $('#bomberman').animate({top: '-=10'}, charSpeed);
+      fromTop --
+      //if the player steps up into a new row, take 8 off the index
+      if (fromTop %5 === 4){
+        index -=8
+      }
+      isIndexBlock()
+      }
+      break;
+    case 'left', 'bomber':
+    if ($('#bomberman').position().left >0){
+      $('#bomberman').animate({left:'-=10'}, charSpeed);
+      fromLeft --
+      //if the player steps left into a column, take 1 off the index
+      if (fromLeft %5 === 4){
+        index --
+      }
+      isIndexBlock()
+    }
+    break;
+    case 'right':
+    if ($('#bomberman').position().left <380){
+      $('#bomberman').animate({left: '+=10'}, charSpeed);
+      fromLeft ++
+      console.log(fromLeft)
+      //if player steps right into a column, add 1 to the index
+      if (fromLeft % 5 ===0){
+        index ++
+      }
+      console.log(index)
+      isIndexBlock()
+    }
+    break;
+  }
+}
 
 //Function that make movement possible -----------------------------------------------------------------
 
 // If there isn't a key pressed at the moment, this happens
 $(document).keydown(function(e){
+  if (!currentKey){
 
-  currentKey = e.keyCode;
+    currentKey = e.keyCode;
 
-  switch(e.keyCode){
-    case 87: charWalk('up', 'bomberman'); break;
-    case 68: charWalk('right', 'bomberman'); break;
-    case 83: charWalk('down', 'bomberman'); break;
-    case 65: charWalk('left', 'bomberman'); break;
-    case 38: charWalk2('up', 'bomberman2'); break;
-    case 39: charWalk2('right', 'bomberman2'); break;
-    case 40: charWalk2('down', 'bomberman2');break;
-    case 37: charWalk2('left', 'bomberman2'); break;
+    switch(e.keyCode){
+      case 87: charWalk('up', 'bomberman'); break;
+      case 68: charWalk('right', 'bomberman'); break;
+      case 83: charWalk('down', 'bomberman'); break;
+      case 65: charWalk('left', 'bomberman'); break;
+    }
   }
 })
 
@@ -245,10 +179,17 @@ function isIndexBlock(){
     player1Score += 100
     $('#player1').html(player1Score)
     $('#block' +allIndexes[index]).removeClass('breakable').addClass('grass')
-    console.log(breakableIndex[index])
     if (player1Score >= 2000){
       $('#container').hide()
       $('h1').html('Well done!!')
+      alert('Player 1 won!')
     }
   }
 }
+
+window.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
--- js/player2.js
+++ js/player2.js
@@ -1,38 +1,111 @@
-window.onload=function(){
-  $('#bomberman2').addClass('down');
-}
+var currentKey2;
+var Timerwalk2;
+var charStep2 = 2;
+var charSpeed2 = 300;
+var player2Score = 0
 
-var currentKey;
-var timerWalk;
-var charSpeed = 300;
-var bomberman = $('#bomberman2');
 
-function processWalk(dir){
-  charStep ++;
-  if (charStep === 5) charStep = 1;
+$(document).keydown(function(m){
+  if (!currentKey2){
+    currentKey2 = m.keyCode;
 
-  //removes class
-  $('#bomberman2').removeAttr('class');
-
-  //add class for direction moving
-  switch(charStep){
-    case 1: $('#bomberman2').addClass(dir); break;
-    case 2: $('#bomberman2').addClass(dir + '-one'); break;
-    case 3: $('#bomberman2').addClass(dir); break;
-    case 4: $('#bomberman2').addClass(dir + '-two'); break;
+    switch (m.keyCode){
+      case 38: charWalk2('up'); break;
+      case 39: charWalk2('right'); break;
+      case 40: charWalk2('down');break;
+      case 37: charWalk2('left'); break;
+    }
   }
+});
+
+$(document).keyup(function(m){
+
+  if (m.keyCode == currentKey2){
+    currentKey2 = false;
+    clearInterval(Timerwalk2);
+    $('#bomberman2').stop(true,true);
+  }
+});
+
+function charWalk2(dir){
+  if (dir == 'up') dir = 'back';
+  if (dir == 'down') dir = 'front';
+
+  processWalk(dir);
 
   Timerwalk2 = setInterval(function(){processWalk2(dir);}, charSpeed);
 }
+function processWalk2(dir){
+  charStep2 ++;
+  if (charStep2 === 5) charStep2=1;
 
+  $('#bomberman2').removeAttr('class');
 
-function isIndexBlock(){
-  if (jQuery.inArray(index, breakableIndex)&& mapArray[index]===1){
-    mapArray[index] = 0
+  switch (charStep2) {
+      case 1: $('#bomberman2').addClass(dir+'2'); break;
+      case 2: $('#bomberman2').addClass(dir+'-one2'); break;
+      case 3: $('#bomberman2').addClass(dir+'2'); break;
+      case 4: $('#bomberman2').addClass(dir+'-two2');  break;
+    }
+    switch(dir) {
+    case'front':
+    if ($('#bomberman2').position().top <370){
+        $('#bomberman2').animate({top: '+=10'}, charSpeed);
+        fromTop2 ++
+        if (fromTop2 % 5 ===0){
+          index2 +=8
+        }
+        isIndexBlock2()
+      }
+      break;
+    case'back':
+      //don't let the character move any further up if they are already at the top of the screen
+      if ($('#bomberman2').position().top > 0) {
+        $('#bombermn2').animate({top: '-=10'}, charSpeed2);
+        fromTop2 --
+        console.log(fromTop2)
+        if (fromTop2 % 5 === 4){
+          index2 -=8
+        }
+        isIndexBlock2();
+        console.log(index2)
+      }
+      break;
+    case'left':
+    //don't let the character move any further left if they are already at the left side of the screen
+    if ($('#bomberman2').position().left > 0) {
+        $('#bomberman2').animate({left: '-=10'}, charSpeed2);
+        fromLeft2 --
+        console.log(fromLeft2)
+        if (fromLeft2 %5 ===4){
+          index2 --
+        }
+        console.log(index2)
+        isIndexBlock2()
+      }
+      break;
+    case'right':
+    if ($('#bomberman2').position().left <380){
+      $('#bomberman2').animate({left: '+=10'}, charSpeed2);
+      fromLeft2 ++
+      console.log(fromLeft2)
+      if (fromLeft%5===0){
+        index2++
+      }
+      console.log(index2)
+      isIndexBlock2
+    }
+      break;
+    }
+}
+
+function isIndexBlock2(){
+  if (jQuery.inArray(index2, breakableIndex)&& mapArray[index2]===1){
+    mapArray[index2] = 0
     player2Score += 100
-    $('#player2').html(player1Score)
-    $('#block' +allIndexes[index]).removeClass('breakable').addClass('grass')
-    console.log(breakableIndex[index])
+    $('#player2').html(player2Score)
+    $('#block' +allIndexes[index2]).removeClass('breakable').addClass('grass')
+    console.log(breakableIndex[index2])
     if (player2Score >= 2000){
       $('#container').hide()
       $('h1').html('Well done!!')
